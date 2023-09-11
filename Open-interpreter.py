3ba26c46aeb1af1bf6636be50b4327f48066b7ce
Himanshu Ninawe <134622172+Himanshu8881212@users.noreply.github.com>
2023-09-11 14:28:50 +0200

    Add files via upload

---
+++ Open-interpreter.py
@@ -0,0 +1,27 @@
+# Define ANSI escape codes for color formatting
+RED = '\033[91m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+ENDC = '\033[0m'
+
+# Header box
+print(f"{BLUE}***********************************************{ENDC}")
+print(f"{GREEN}*              Open-Interpreter              *{ENDC}")
+print(f"{BLUE}***********************************************{ENDC}")
+print("\n\n")  # Add two empty lines
+
+import interpreter
+interpreter.local = True
+#interpreter.model = "gpt-3.5-turbo"
+interpreter.auto_run = True
+
+while True:
+    print(f"{RED}You:{ENDC}", end="")
+    user_input = input() 
+    # Exit the loop if the user types 'exit'
+    if user_input.lower() == 'exit':
+        break
+    
+    print(f"{YELLOW}O/I:{ENDC}", )
+    interpreter.chat(user_input)
